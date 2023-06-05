8de997bf06833cb0778022d33a113e8435cc7bd6
Hemantt07 <reshavdhiman67@gmail.com>
2023-06-05 16:27:06 +0530

    ds

--- api/index.js
+++ api/index.js
@@ -2,22 +2,29 @@ const express =  require("express");
 
 const app = express();
 
-const mongoose = require("mongoose");
-
-const connectToMongo = async () => {
-    mongoose.connect( 
-        process.env.MONGO_URI ,
-        { useNewUrlParser: true, useUnifiedTopology: true },
-        () => {
-            console.log("Connected to mongo Successful");
-        }
-)};
-
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const { MongoClient } = require("mongodb");
 
 dotenv.config();
+
+
+const client = new MongoClient(process.env.MONGO_URL);
+async function connectDB() {
+    await client.connect();
+    console.log("Successfully connected to Atlas");
+}
+connectDB();
+
+// Middlewares
+
+app.use(express.json());
+app.use(helmet());
+app.use(morgan('common'));
+
+
+
 
 app.listen(8800, () => {
     console.log("Server");
